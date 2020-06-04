529baaf523d5862a9294a625750cc8af6dbd7903
Noelia Albornoz <albornoznoeliaunlam@gmail.com>
2020-06-04 13:01:03 -0300

    Se creó la clase ReservaManager con sus atributos y métodos, similares a HuespedManager.

---
+++ hoteltresvagos/src/main/java/ar/com/ada/hoteltresvagos/managers/ReservaManager.java
@@ -0,0 +1,126 @@
+package ar.com.ada.hoteltresvagos.managers;
+
+import java.util.List;
+import java.util.logging.Level;
+
+import javax.persistence.Query;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+
+import ar.com.ada.hoteltresvagos.entities.*;
+
+public class ReservaManager {
+
+    protected SessionFactory sessionFactory;
+
+    public void setup() {
+
+        java.util.logging.Logger.getLogger("org.hibernate").setLevel(Level.OFF);
+        final StandardServiceRegistry registry = new StandardServiceRegistryBuilder().configure() // configures settings
+                                                                                                  // from
+                                                                                                  // hibernate.cfg.xml
+                .build();
+        try {
+            sessionFactory = new MetadataSources(registry).buildMetadata().buildSessionFactory();
+        } catch (Exception ex) {
+            StandardServiceRegistryBuilder.destroy(registry);
+            throw ex;
+        }
+
+    }
+
+    public void exit() {
+        sessionFactory.close();
+    }
+
+    public void create(Reserva reserva) {
+
+        Session session = sessionFactory.openSession();
+        session.beginTransaction();
+
+        session.save(reserva);
+
+        session.getTransaction().commit();
+        session.close();
+    }
+
+    public Reserva read(int reservaId) {
+        Session session = sessionFactory.openSession();
+
+        Reserva reserva = session.get(Reserva.class, reservaId);
+
+        session.close();
+
+        return reserva;
+    }
+
+    public void update(Reserva reserva) {
+
+        Session session = sessionFactory.openSession();
+        session.beginTransaction();
+
+        session.update(reserva);
+
+        session.getTransaction().commit();
+        session.close();
+    }
+
+    public void delete(Reserva reserva) {
+
+        Session session = sessionFactory.openSession();
+        session.beginTransaction();
+
+        session.delete(reserva);
+
+        session.getTransaction().commit();
+        session.close();
+    }
+
+    /**
+     * Este metodo en la vida real no debe existir ya qeu puede haber miles de
+     * usuarios
+     * 
+     * @return
+     */
+    public List<Reserva> buscarTodas() {
+
+        Session session = sessionFactory.openSession();
+
+        /// NUNCA HARCODEAR SQLs nativos en la aplicacion.
+        // ESTO es solo para nivel educativo
+        Query query = session.createNativeQuery("SELECT * FROM reserva", Reserva.class);
+        //query = session.createQuery("From Obse")
+        List<Reserva> todas = query.getResultList();
+
+        return todas;
+
+    }
+
+    /**
+     * Busca una lista de huespedes por el nombre completo Esta armado para que se
+     * pueda generar un SQL Injection y mostrar commo NO debe programarse.
+     * 
+     * @param nombre
+     * @return
+     */
+    public List<Reserva> buscarPor(int huespedId) {
+
+        Session session = sessionFactory.openSession();
+
+        // SQL Injection vulnerability exposed.
+        // Deberia traer solo aquella del nombre y con esto demostrarmos que trae todas
+        // si pasamos
+        // como nombre: "' or '1'='1"
+        Query query = session.createNativeQuery("SELECT * FROM reserva where huesped_id = '" + huespedId + "'", Reserva.class);
+
+        List<Reserva> reservas = query.getResultList();
+
+        return reservas;
+
+    }
+
+}
