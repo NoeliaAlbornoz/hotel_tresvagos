69ec7eed4dfc07c88a36af63ef176f020a71c59a
Noelia Albornoz <albornoznoeliaunlam@gmail.com>
2020-06-08 09:29:23 -0300

    Minor fix.

--- hoteltresvagos/src/main/java/ar/com/ada/hoteltresvagos/App.java
+++ hoteltresvagos/src/main/java/ar/com/ada/hoteltresvagos/App.java
@@ -1,9 +1,5 @@
 package ar.com.ada.hoteltresvagos;
 
-/**
- * Hello world!
- *
- */
 public class App {
     public static void main(String[] args) throws Exception {
         ABM miABM = new ABM();
