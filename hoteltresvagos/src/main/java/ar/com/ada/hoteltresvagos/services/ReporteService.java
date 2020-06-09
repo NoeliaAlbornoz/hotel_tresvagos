758db763543a9dcd9269791e0d46bf1fbce5455c
Noelia Albornoz <albornoznoeliaunlam@gmail.com>
2020-06-09 10:14:06 -0300

    Se completó clase ReporteService.

--- hoteltresvagos/src/main/java/ar/com/ada/hoteltresvagos/services/ReporteService.java
+++ hoteltresvagos/src/main/java/ar/com/ada/hoteltresvagos/services/ReporteService.java
@@ -1,5 +1,73 @@
 package ar.com.ada.hoteltresvagos.services;
 
+import java.util.List;
+import java.util.Scanner;
+
+import ar.com.ada.hoteltresvagos.entities.reportes.ReporteImportesEstado;
+import ar.com.ada.hoteltresvagos.entities.reportes.ReporteImportesHuesped;
+import ar.com.ada.hoteltresvagos.managers.ReporteManager;
+
 public class ReporteService {
+
+    public static Scanner Teclado = new Scanner(System.in);
+    protected ReporteManager ABMReporte = new ReporteManager();
+
+    public int ingresarID() {
+
+        System.out.println("Ingrese el ID: ");
+
+        int id = Teclado.nextInt();
+        Teclado.nextLine();
+
+        return id;
+
+    }
+
+    public void listarPorHuespedId() {
+
+        int id = ingresarID();
+        List<ReporteImportesHuesped> reportes = ABMReporte.generarPorHuespedId(id);
+        for (ReporteImportesHuesped reporte : reportes) {
+            mostrar(reporte);
+        }
+    }
+
+    public void listarPorHuespedes() {
+
+        List<ReporteImportesHuesped> reportes = ABMReporte.generarPorHuespedes();
+        for (ReporteImportesHuesped reporte : reportes) {
+            mostrar(reporte);
+        }
+    }
+
+    public void listarPorEstados() {
+
+        List<ReporteImportesEstado> reportes = ABMReporte.generarPorEstados();
+        for (ReporteImportesEstado reporte : reportes) {
+            mostrar(reporte);
+        }
+    }
+
+    public void listarPorEstadoId() {
+
+        int id = ingresarID();
+        List<ReporteImportesEstado> reportes = ABMReporte.generarPorEstadoId(id);
+        for (ReporteImportesEstado reporte : reportes) {
+            mostrar(reporte);
+        }
+    }
+
+    public void mostrar(ReporteImportesHuesped reporte) {
+
+        System.out.println("Id " + reporte.getHuespedId() + "Cantidad de Reservas " + reporte.getCantidadReservas() +  "Importe de Reserva " + reporte.getImporteReserva() + "Importe Total " + reporte.getImporteTotal() + "Importe Pagado " + reporte.getImportePagado());
+
+    }
+
+    public void mostrar(ReporteImportesEstado reporte) {
+
+        System.out.println("Id " + reporte.getEstadoId() + "Cantidad de Reservas " + reporte.getCantidadReservas() +  "Importe de Reserva " + reporte.getImporteReserva() + "Importe Total " + reporte.getImporteTotal() + "Importe Pagado " + reporte.getImportePagado());
+
+    }
+
     
 }
